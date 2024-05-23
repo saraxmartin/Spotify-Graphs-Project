8ec66b227fee039f96eed4a47a8a33f56d1e95f2
Sara Martín <127147277+saraxmartin@users.noreply.github.com>
2024-05-23 15:12:29 +0200

    Add files via upload

---
+++ Lab_AGX_202324_P2_skeleton.py
@@ -0,0 +1,81 @@
+import networkx as nx
+import pandas as pd
+
+# ------- IMPLEMENT HERE ANY AUXILIARY FUNCTIONS NEEDED ------- #
+
+
+# --------------- END OF AUXILIARY FUNCTIONS ------------------ #
+
+def retrieve_bidirectional_edges(g: nx.DiGraph, out_filename: str) -> nx.Graph:
+    """
+    Convert a directed graph into an undirected graph by considering bidirectional edges only.
+
+    :param g: a networkx digraph.
+    :param out_filename: name of the file that will be saved.
+    :return: a networkx undirected graph.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def prune_low_degree_nodes(g: nx.Graph, min_degree: int, out_filename: str) -> nx.Graph:
+    """
+    Prune a graph by removing nodes with degree < min_degree.
+
+    :param g: a networkx graph.
+    :param min_degree: lower bound value for the degree.
+    :param out_filename: name of the file that will be saved.
+    :return: a pruned networkx graph.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def prune_low_weight_edges(g: nx.Graph, min_weight=None, min_percentile=None, out_filename: str = None) -> nx.Graph:
+    """
+    Prune a graph by removing edges with weight < threshold. Threshold can be specified as a value or as a percentile.
+
+    :param g: a weighted networkx graph.
+    :param min_weight: lower bound value for the weight.
+    :param min_percentile: lower bound percentile for the weight.
+    :param out_filename: name of the file that will be saved.
+    :return: a pruned networkx graph.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def compute_mean_audio_features(tracks_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Compute the mean audio features for tracks of the same artist.
+
+    :param tracks_df: tracks dataframe (with audio features per each track).
+    :return: artist dataframe (with mean audio features per each artist).
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def create_similarity_graph(artist_audio_features_df: pd.DataFrame, similarity: str, out_filename: str = None) -> \
+        nx.Graph:
+    """
+    Create a similarity graph from a dataframe with mean audio features per artist.
+
+    :param artist_audio_features_df: dataframe with mean audio features per artist.
+    :param similarity: the name of the similarity metric to use (e.g. "cosine" or "euclidean").
+    :param out_filename: name of the file that will be saved.
+    :return: a networkx graph with the similarity between artists as edge weights.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+if __name__ == "__main__":
+    # ------- IMPLEMENT HERE THE MAIN FOR THIS SESSION ------- #
+    pass
+    # ------------------- END OF MAIN ------------------------ #
