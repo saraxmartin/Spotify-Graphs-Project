923eb0356aef44ec78de308d4f165cce485d95fb
Sara Martín <sara.04.sm@gmail.com>
2024-05-30 11:42:41 +0200

    added pdfs

---
+++ Lab_AGX_202324_P4_skeleton.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+# ------- IMPLEMENT HERE ANY AUXILIARY FUNCTIONS NEEDED ------- #
+
+
+# --------------- END OF AUXILIARY FUNCTIONS ------------------ #
+
+def plot_degree_distribution(degree_dict: dict, normalized: bool = False, loglog: bool = False) -> None:
+    """
+    Plot degree distribution from dictionary of degree counts.
+
+    :param degree_dict: dictionary of degree counts (keys are degrees, values are occurrences).
+    :param normalized: boolean indicating whether to plot absolute counts or probabilities.
+    :param loglog: boolean indicating whether to plot in log-log scale.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def plot_audio_features(artists_audio_feat: pd.DataFrame, artist1_id: str, artist2_id: str) -> None:
+    """
+    Plot a (single) figure with a plot of mean audio features of two different artists.
+
+    :param artists_audio_feat: dataframe with mean audio features of artists.
+    :param artist1_id: string with id of artist 1.
+    :param artist2_id: string with id of artist 2.
+    :return: None
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+def plot_similarity_heatmap(artist_audio_features_df: pd.DataFrame, similarity: str, out_filename: str = None) -> None:
+    """
+    Plot a heatmap of the similarity between artists.
+
+    :param artist_audio_features_df: dataframe with mean audio features of artists.
+    :param similarity: string with similarity measure to use.
+    :param out_filename: name of the file to save the plot. If None, the plot is not saved.
+    """
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+    pass
+    # ----------------- END OF FUNCTION --------------------- #
+
+
+if __name__ == "__main__":
+    # ------- IMPLEMENT HERE THE MAIN FOR THIS SESSION ------- #
+    pass
+    # ------------------- END OF MAIN ------------------------ #
