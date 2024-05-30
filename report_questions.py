dd5ab9e245b4424fd64e90a472f8fda24369a950
Sara Martín <sara.04.sm@gmail.com>
2024-05-30 17:03:41 +0200

    new bidir graphs

--- report_questions.py
+++ report_questions.py
@@ -1,48 +1,47 @@
 import statistics
-
+import networkx as nx
 
 # ----------------- PART 1 --------------------- #
 
 def dataset_info(df):
-  num_songs = len(df)
-  num_artists = df['artist_id'].nunique()
-  num_albums = df['album_id'].nunique()
-  print("Number of songs:", num_songs)
-  print("Number of different artists:", num_artists)
-  print("Number of different albums:", num_albums)
-  return
+    num_songs = len(df)
+    num_artists = df['artist_id'].nunique()
+    num_albums = df['album_id'].nunique()
+    print("Number of songs:", num_songs)
+    print("Number of different artists:", num_artists)
+    print("Number of different albums:", num_albums)
+
 
 def graph_info(G):
-  print("Order:", G.order())
-  print("Size:", G.size())
+    print("Order:", G.order())
+    print("Size:", G.size())
 
-  # Calculate in-degrees and out-degrees
-  in_degrees = [d for n, d in G.in_degree()]
-  out_degrees = [d for n, d in G.out_degree()]
+    # Calculate in-degrees and out-degrees
+    in_degrees = [d for n, d in G.in_degree()]
+    out_degrees = [d for n, d in G.out_degree()]
 
-  # Calculate minimum, maximum, and median of in-degrees
-  min_in_degree = min(in_degrees)
-  max_in_degree = max(in_degrees)
-  median_in_degree = statistics.median(in_degrees)
+    # Calculate minimum, maximum, and median of in-degrees
+    min_in_degree = min(in_degrees)
+    max_in_degree = max(in_degrees)
+    median_in_degree = statistics.median(in_degrees)
 
-  # Calculate minimum, maximum, and median of out-degrees
-  min_out_degree = min(out_degrees)
-  max_out_degree = max(out_degrees)
-  median_out_degree = statistics.median(out_degrees)
+    # Calculate minimum, maximum, and median of out-degrees
+    min_out_degree = min(out_degrees)
+    max_out_degree = max(out_degrees)
+    median_out_degree = statistics.median(out_degrees)
 
-  print("\nIN-DEGREE:")
-  print("Minimum:", min_in_degree)
-  print("Maximum:", max_in_degree)
-  print("Median:", median_in_degree)
-  print("In-degrees:", sorted(in_degrees))
+    print("\nIN-DEGREE:")
+    print("Minimum:", min_in_degree)
+    print("Maximum:", max_in_degree)
+    print("Median:", median_in_degree)
+    #print("In-degrees:", sorted(in_degrees))
 
-  print("\nOUT-DEGREE:")
-  print("Minimum:", min_out_degree)
-  print("Maximum:", max_out_degree)
-  print("Median:", median_out_degree)
-  print("Out-degrees:", sorted(out_degrees))
+    print("\nOUT-DEGREE:")
+    print("Minimum:", min_out_degree)
+    print("Maximum:", max_out_degree)
+    print("Median:", median_out_degree)
+    #print("Out-degrees:", sorted(out_degrees))
 
-  return
 
 
 # ----------------- PART 2 --------------------- #
@@ -55,3 +54,20 @@ def graph_info(G):
 
 
 # ----------------- PART 4 --------------------- #
+
+
+
+
+
+# ------------ ANSWER QUESTIONS------------------ #
+if __name__ == "__main__":
+
+    # Import graphs
+    gb = nx.read_graphml("./graphs/gB")
+    gd = nx.read_graphml("./graphs/gD")
+
+    # PART 1: DATA ADQUISITION
+    print("#----------Info about GB:------------#\n")
+    graph_info(gb)
+    print("\n#----------Info about GD:------------#")
+    graph_info(gd)
